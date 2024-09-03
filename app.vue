ab9ceb419150362fb235f602801b3c6f3aef5dd1
Jimena <j.miguez@airlst.com>
2024-09-03 09:52:10 +0200

    organize folder structure

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
   <section class="container">
-    <p class="text-2xl text-brand-500 font-bold font-body">Boilerplate Basic</p>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </section>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <section class="mt-10 text-brand-500">
+    <p>Boilrplate Basic</p>
+  </section>
+</template>
+
+<script setup></script>
